5dedf3d00950711957111d56d55ce4f6bb6bb8ed
Josh Ellis <joshua.ellis18@gmail.com>
2019-11-24 16:07:20 +0000

    remove analytics as tag manager adds this for you.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,17 +1,12 @@
 /* eslint-disable react/react-in-jsx-scope */
 import Document, { Head, Main, NextScript } from 'next/document';
-import {
-  GAnalytics,
-  GTMTrackingHead,
-  GTMTrackingBody,
-} from '../components/Tracking';
+import { GTMTrackingHead, GTMTrackingBody } from '../components/Tracking';
 
 export default class extends Document {
   render() {
     return (
       <html lang="en">
         <Head>
-          <GAnalytics />
           <GTMTrackingHead />
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
         </Head>
