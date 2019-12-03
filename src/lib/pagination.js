d8c1549cee831f08564929ef8e638945781e2953
Josh Ellis <joshua.ellis18@gmail.com>
2019-12-03 23:00:19 +0000

    [snag] can't render projects that aren't in project structure using slug

--- src/lib/pagination.js
+++ src/lib/pagination.js
@@ -1,16 +1,22 @@
 export default function getNextPrevItems(current, array) {
   let paginationItems;
+  console.log('pagin >>>',current, array);
   array.forEach((item, index) => {
     if (item.slug === current && index) {
       paginationItems = [array[index - 1], array[index + 1]];
     }
     if (item.slug === current && index === 0) {
-      paginationItems = [array[array.length - 1], array[1]];
+      paginationItems = [array[array.length - 1], array[index + 1]];
     }
     if (item.slug === current && index === array.length - 1) {
-      paginationItems = [array[array.length - 2], array[0]];
+      paginationItems = [array[index - 1], array[0]];
     }
   });
+
+  // return the last item and the first item if nothing is there
+  if (!paginationItems) {
+    paginationItems = [array[array.length - 1], array[0]];
+  }
 
   return paginationItems;
 }
