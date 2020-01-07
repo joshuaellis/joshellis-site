87fd5b38344f819561474d19f73a783fd29cf509
Josh Ellis <joshua.ellis18@gmail.com>
2020-01-07 23:57:48 +0000

    update nextjs and _document

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,20 +1,27 @@
 /* eslint-disable react/react-in-jsx-scope */
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { GTMTrackingHead, GTMTrackingBody } from '../components/Tracking';
 
 export default class extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
-      <html lang="en">
+      <Html lang="en">
         <Head>
+          <GTMTrackingHead />
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
         </Head>
         <body>
+          <GTMTrackingBody />
           <Main />
           <div id="modal" />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
