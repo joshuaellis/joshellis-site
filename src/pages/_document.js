364dfb407cc0a8a69ea6c4e1ee4edea92ea34813
Josh Ellis <joshua.ellis18@gmail.com>
2020-01-08 07:56:17 +0000

    Revert "update nextjs and _document"
    This reverts commit 87fd5b38344f819561474d19f73a783fd29cf509.

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,27 +1,20 @@
 /* eslint-disable react/react-in-jsx-scope */
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Head, Main, NextScript } from 'next/document';
 import { GTMTrackingHead, GTMTrackingBody } from '../components/Tracking';
 
 export default class extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
-  }
-
   render() {
     return (
-      <Html lang="en">
+      <html lang="en">
         <Head>
-          <GTMTrackingHead />
           <link rel="icon" type="image/x-icon" href="/static/favicon.ico" />
         </Head>
         <body>
-          <GTMTrackingBody />
           <Main />
           <div id="modal" />
           <NextScript />
         </body>
-      </Html>
+      </html>
     );
   }
 }
