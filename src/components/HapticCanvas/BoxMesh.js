06ace5bd77c03975b434f2ccf5903b2625707753
Josh Ellis <joshua.ellis18@gmail.com>
2021-01-07 18:51:03 +0000

    feat: add interaction with placement of wave

--- src/components/HapticCanvas/BoxMesh.js
+++ src/components/HapticCanvas/BoxMesh.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useLayoutEffect, useRef } from 'react'
 import { useTweaks, makeSeparator } from 'use-tweaks'
 import * as THREE from 'three'
 import { useFrame } from 'react-three-fiber'
@@ -11,6 +11,8 @@ export default function BoxGrid ({ gridSize = [96, 72] }) {
   const TOTAL_BOXES = gridSizeX * gridSizeY
   const meshRef = useRef()
 
+  const waves = useRef([])
+
   const {
     amplitude,
     frequency,
@@ -21,31 +23,62 @@ export default function BoxGrid ({ gridSize = [96, 72] }) {
     dampen,
     colorIntensity
   } = useTweaks('wave', {
-    frequency: { value: 0.7, min: 0, max: 1 },
-    amplitude: { value: 0.17, min: 0, max: 2 },
-    crest: { value: 0.45, min: 0, max: 1 },
-    speed: { value: 4.1, min: 1, max: 20 },
-    dampen: { value: 1.1, min: 0, max: 1.2 },
+    frequency: { value: 0.89, min: 0, max: 1 },
+    amplitude: { value: 0.48, min: 0, max: 2 },
+    crest: { value: 0.42, min: 0, max: 1 },
+    speed: { value: 5.9, min: 1, max: 20 },
+    dampen: { value: 1.43, min: 0, max: 2 },
     ...makeSeparator(),
     hue: { value: 234, min: 0, max: 360 },
     saturation: { value: 100, min: 0, max: 100 },
     colorIntensity: { value: 1.04, min: 1, max: 5 }
   })
 
-  const cx = gridSizeX / 2
-  const cy = gridSizeY / 2
-
-  useFrame(({ clock }) => {
-    const time = clock.getElapsedTime()
+  const polarCubes = React.useMemo(() => {
+    let cubes = []
     let totalCount = 0
 
     for (let x = 0; x < gridSizeX; x++) {
       for (let y = 0; y < gridSizeY; y++) {
         const id = totalCount++
-        // const posZ = waveNoise(posX + Math.sin(time), posY)
+        cubes = [...cubes, { x, y, id }]
+      }
+    }
 
-        const vel = Math.sqrt(Math.pow(x - cx, 2) + Math.pow(y - cy, 2))
+    return cubes
+  }, [])
 
+  const createWave = (cx, cy) => {
+    waves.current = [
+      polarCubes.map(cube => ({
+        ...cube,
+        vel: Math.sqrt(Math.pow(cube.x - cx, 2) + Math.pow(cube.y - cy, 2))
+      }))
+    ]
+  }
+
+  const handlePointerDown = e => {
+    const { x, y } = polarCubes[e.instanceId]
+    createWave(x, y)
+  }
+
+  useLayoutEffect(() => {
+    polarCubes.forEach(({ x, y, id }) => {
+      tempObject.position.set(x, y, 0)
+
+      tempObject.updateMatrix()
+
+      meshRef.current.setMatrixAt(id, tempObject.matrix)
+
+      meshRef.current.setColorAt(id, tempColor.setHSL(360, 1, 1))
+    })
+  }, [])
+
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime()
+
+    waves.current.forEach(cubes => {
+      cubes.forEach(({ x, vel, y, id }) => {
         const l =
           amplitude *
             Math.pow(Math.E, (-dampen / 10) * vel) *
@@ -53,7 +86,11 @@ export default function BoxGrid ({ gridSize = [96, 72] }) {
           1 -
           crest
 
-        tempObject.position.set(x, y, (1 - l) * 20)
+        const isPastLimit = l <= 0.56
+
+        const posZ = isPastLimit ? l : 0.5
+
+        tempObject.position.set(x, y, posZ * 5)
 
         tempObject.updateMatrix()
 
@@ -64,19 +101,25 @@ export default function BoxGrid ({ gridSize = [96, 72] }) {
           tempColor.setHSL(
             hue / 360,
             saturation / 100,
-            l <= dampen / 2 ? l / colorIntensity : 1
+            isPastLimit ? l / colorIntensity : 1
           )
         )
-      }
-    }
+      })
+    })
 
-    meshRef.current.instanceColor.needsUpdate = true
-    meshRef.current.instanceMatrix.needsUpdate = true
+    if (waves.current.length > 0) {
+      meshRef.current.instanceColor.needsUpdate = true
+      meshRef.current.instanceMatrix.needsUpdate = true
+    }
   })
 
   return (
-    <group position={[-cx, -cy, 0]}>
-      <instancedMesh ref={meshRef} args={[null, null, TOTAL_BOXES]}>
+    <group position={[-gridSizeX / 2, -gridSizeY / 2, 0]}>
+      <instancedMesh
+        ref={meshRef}
+        args={[null, null, TOTAL_BOXES]}
+        onPointerDown={handlePointerDown}
+      >
         <boxBufferGeometry
           attach='geometry'
           args={[1, 1, 1]}
